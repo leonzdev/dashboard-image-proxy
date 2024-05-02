50c01cb0ae629cdebd26eb39b6d9a72d443f9884
leonzdev <leonzdev@leonz.dev>
2024-05-02 14:47:45 -0400

    [untested] fix some config bug that has perf impact

--- app/config.ts
+++ app/config.ts
@@ -16,8 +16,8 @@ const globalConfig: AppConfig = {
     dashboardUrl: process.env.DASH_URL || fatal('DASH_URL is required'),
     authHeaderKey: process.env.AUTH_HEADER_KEY || DEFAULT_AUTH_HEADER_KEY,
     authHeaderValue: process.env.AUTH_HEADER_VALUE || fatal('AUTH_HEADER_VALUE is required'),
-    isHeadless: process.env.HEADLESS ? Boolean(process.env.HEADLESS) : false,
-    shouldReloadBeforeScreenshot: process.env.RELOAD ? Boolean(process.env.RELOAD) : false,
+    isHeadless: process.env.HEADLESS ? process.env.HEADLESS.toLocaleLowerCase() === 'true' : false,
+    shouldReloadBeforeScreenshot: process.env.RELOAD ? process.env.RELOAD.toLocaleLowerCase() === 'true' : false,
 }
 
 export default globalConfig
